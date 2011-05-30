6627513f85a20bb6a3d2e9157c78f15953b5f68b
pandawebstudio@gmail.com <pandawebstudio@gmail.com@62dcb464-03f7-fedf-af63-78162ab3ff31>
2011-05-30 17:46:19 +0000

    B: Search grid by product name error Fixed search by product name/sort by product name in admin product QA grid

--- app/code/community/PWS/ProductQA/Block/Adminhtml/Productqa/Grid.php
+++ app/code/community/PWS/ProductQA/Block/Adminhtml/Productqa/Grid.php
@@ -10,7 +10,7 @@ class PWS_ProductQA_Block_Adminhtml_Productqa_Grid extends Mage_Adminhtml_Block_
         $this->setDefaultDir('DESC');
         $this->setSaveParametersInSession(true);
     }
-    
+
     protected function _getStore()
     {
         $storeId = (int) $this->getRequest()->getParam('store', 0);
@@ -18,24 +18,44 @@ class PWS_ProductQA_Block_Adminhtml_Productqa_Grid extends Mage_Adminhtml_Block_
     }
 
     protected function _prepareCollection()
-    { 
+    {
         $store = $this->_getStore();
-        
+
         $collection = Mage::getModel('pws_productqa/productqa')
                             ->getCollection()
                             ->joinProducts()
                             ->setOrder('answered_on', 'ASC')
                             ->setOrder('created_on', 'ASC')
-                      ;  
-                          
-        if ($store->getId()) {            
+                      ;
+
+        if ($store->getId()) {
             $collection->addFieldToFilter('main_table.store_id', $store->getId());
-        }    
-        
-        //echo $collection->getSelect()->__toString();        
-                        
+        }
+
+        //echo $collection->getSelectSql();
+
         $this->setCollection($collection);
-        return parent::_prepareCollection();
+        $columnId = $this->getParam($this->getVarNameSort(), $this->_defaultSort);
+
+        // make it possible to sort by product_name
+        if ($columnId == 'product_name' ) {
+            $this->_preparePage();
+
+            $columnId = $this->getParam($this->getVarNameSort(), $this->_defaultSort);
+            $dir      = $this->getParam($this->getVarNameDir(), $this->_defaultDir);
+
+
+            if (isset($this->_columns[$columnId]) && $this->_columns[$columnId]->getIndex()) {
+                $dir = (strtolower($dir)=='desc') ? 'desc' : 'asc';
+                $this->_columns[$columnId]->setDir($dir);
+                $collection->getSelect()->order('IFNULL(_table_product_name.value,_table_product_name2.value) '.$dir);
+            }
+
+        } 
+
+        parent::_prepareCollection();
+
+        return $this;
     }
 
     protected function _prepareColumns()
@@ -53,39 +73,40 @@ class PWS_ProductQA_Block_Adminhtml_Productqa_Grid extends Mage_Adminhtml_Block_
             'type'		=> 'datetime',
             'index'     => 'created_on',
         ));
-        
+
         $this->addColumn('product_name', array(
             'header'    => Mage::helper('pws_productqa')->__('Product Name'),
             'align'     =>'left',
             'index'     => 'product_name',
+            'filter_condition_callback' => array($this, '_filterProductNameCondition')
         ));
-        
+
         $this->addColumn('name', array(
             'header'    => Mage::helper('pws_productqa')->__('Name'),
             'align'     =>'left',
             'index'     => 'name',
         ));
-        
+
         $this->addColumn('email', array(
             'header'    => Mage::helper('pws_productqa')->__('Email'),
             'align'     =>'left',
             'index'     => 'email',
         ));
-        
+
         $this->addColumn('question', array(
             'header'    => Mage::helper('pws_productqa')->__('Question'),
             'align'     =>'left',
             'index'     => 'question',
         ));
-        
-        
+
+
         $this->addColumn('answered_on', array(
             'header'    => Mage::helper('pws_productqa')->__('Answered On'),
             'align'     =>'left',
             'index'     => 'answered_on',
             'type'		=> 'datetime',
         ));
-        
+
         $this->addColumn('status', array(
             'header'    => Mage::helper('pws_productqa')->__('Status'),
             'align'     =>'left',
@@ -96,8 +117,19 @@ class PWS_ProductQA_Block_Adminhtml_Productqa_Grid extends Mage_Adminhtml_Block_
 
         return parent::_prepareColumns();
     }
-    
-    
+
+
+    protected function _filterProductNameCondition($collection, $column)
+    {
+        if (!$value = $column->getFilter()->getValue()) {
+            return;
+        }
+        $this->getCollection()->getSelect()
+            ->where('`_table_product_name`.`value` like ?', '%'.$value.'%')
+            ->orWhere('`_table_product_name2`.`value` like ?', '%'.$value.'%');
+    }
+
+
     protected function _prepareMassaction()
     {
         $this->setMassactionIdField('productqa_id');
@@ -109,16 +141,16 @@ class PWS_ProductQA_Block_Adminhtml_Productqa_Grid extends Mage_Adminhtml_Block_
              'confirm'  => Mage::helper('pws_productqa')->__('Are you sure?')
         ));
 
-       
+
         return $this;
     }
-    
-    
+
+
     public function getRowUrl($row)
     {
         return $this->getUrl('*/*/edit', array('id' => $row->getId()));
     }
 
-    
+
 
 }
